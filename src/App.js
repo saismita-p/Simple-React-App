712431efa279411a219702a02263816b771fdea0
saismita-p <saismita.pattam@gmail.com>
2025-03-06 14:26:19 +0530

    lazy loading of Grocery component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -25,6 +25,8 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
  *    - Contact
  */
 
+//Chunking, Code Splitting, Lazy Loading. Dynamic loading [Grocery]...
+const Grocery = lazy(() => import("./components/Grocery"));
 const AppLayout = () => {
   return (
     <div className="app">
@@ -50,6 +52,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+  return <h1>Order Grocery Online!</h1>;
+};
+
+export default Grocery;
